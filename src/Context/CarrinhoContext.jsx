385d052a31c70394b14ce2380550e146743b4ae2
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-02-26 11:37:51 -0300

    .

--- src/Context/CarrinhoContext.jsx
+++ src/Context/CarrinhoContext.jsx
@@ -1,11 +1,44 @@
-import { createContext } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
+import { carrinhoReducer } from "../reducers/carrinhoReducer";
 
-const CarrinhoContext = createContext();
+export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = "Carrinho";
 
-export const CarrinhoProvider = () => {
-    return(
-        <CarrinhoContext.Provider>
-            {children}
-        </CarrinhoContext.Provider>
-    )
-}
+const estadoInicial = [];
+
+export const CarrinhoProvider = ({ children }) => {
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
+  const [quantidade, setQuantidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
+
+  const { totalTemp, quantidadeTemp } = useMemo(() => {
+    return carrinho.reduce(
+      (acumulador, produto) => ({
+        quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+        totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+      }),
+      {
+        quantidadeTemp: 0,
+        totalTemp: 0,
+      }
+    );
+  }, [carrinho]);
+
+  useEffect(() => {
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  });
+
+  return (
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        dispatch,
+        quantidade,
+        valorTotal,
+      }}
+    >
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
